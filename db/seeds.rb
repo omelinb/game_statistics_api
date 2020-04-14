b8ce1b609ca9380653d2cee3bf181265d69957b4
omelinb <elros.ob@gmail.com>
2020-04-14 14:47:19 +0300

    Added frozen_string_literal

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,8 @@
 Player.destroy_all
 Team.destroy_all
 
-2.times do
-  Team.create!(name: Faker::Team.name)
-end
+2.times { Team.create!(name: Faker::Team.name) }
 
 Team.all.each do |team|
-  5.times do
-    team.players.create(name: Faker::Name.name)
-  end
+  5.times { team.players.create(name: Faker::Name.name) }
 end
