9cef8253f520730b18d74e7a7d580b9f95115ed6
omelinb <elros.ob@gmail.com>
2020-04-14 17:11:26 +0300

    Added GameStatistic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-Achievement.destroy_all
 Game.destroy_all
+Achievement.destroy_all
 Player.destroy_all
 Team.destroy_all
+
 
 Achievement.create!(description: 'Ran 10+ kilometers')
 Achievement.create!(description: 'Made 70% accurate throws')
@@ -20,3 +21,13 @@ Team.all.each do |team|
   end
 end
 
+Game.all.each do |game|
+  Player.all.each do |player|
+    GameStatistic.create!(
+      game_id: game.id,
+      player_id: player.id,
+      team_id: player.team.id,
+      achievement_id: [*Achievement.ids, nil]
+    )
+  end
+end
